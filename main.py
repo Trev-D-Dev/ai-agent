4c25db08e3c7ee975b3bb07971dca3b190d764df
Trev-D-Dev <dunn.trevor@pm.me>
2025-06-10 15:57:50 -0400

    Started AI Agent Project

---
+++ main.py
@@ -0,0 +1,26 @@
+import os
+from dotenv import load_dotenv
+from google import genai
+import sys
+from google.genai import types
+
+load_dotenv()
+api_key = os.environ.get("GEMINI_API_KEY")
+
+client = genai.Client(api_key=api_key)
+
+if(len(sys.argv) == 1):
+    print("ERROR: please provide prompt")
+    sys.exit(1)
+
+user_prompt = sys.argv[1]
+messages = [
+    types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+]
+response = client.models.generate_content(
+    model="gemini-2.0-flash-001", contents=messages
+)
+
+print(response.text)
+print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
